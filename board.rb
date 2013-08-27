051689d4726094b7527a8716296db74fc3c173e0
App Academy Student <student@appacademy.io>
2013-08-27 14:28:11 -0400

    Adds knight class

--- board.rb
+++ board.rb
@@ -7,9 +7,9 @@ class Board
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
-    self[0, 0] = Queen.new(:white, 0, 0, self)
-    self[0, 3] = Queen.new(:white, 0, 3, self)
-    self[4, 4] = Queen.new(:black, 4, 4, self)
+    self[0, 0] = Knight.new(:white, 0, 0, self)
+    self[0, 3] = Knight.new(:white, 0, 3, self)
+    self[2, 1] = Knight.new(:black, 2, 1, self)
   end
 
   def [](row, col)
@@ -48,7 +48,7 @@ end
 
 game = Board.new
 game.display
-game[0, 0].make_move(4, 4)
+game[0, 0].make_move(2, 1)
 game.display
-game[4, 4].make_move(1, 1)
+game[4, 4].make_move(3, 2)
 game.display
--- piece.rb
+++ piece.rb
@@ -199,3 +199,31 @@ class Queen < Piece
     diagonal || straight
   end
 end
+
+class Knight < Piece
+  def initialize(color, row, col, board)
+    color == :black ? label = :K : label = :k
+    color == :black ? @unicode = "\u265E" : @unicode = "\u2658"
+    super(color, row, col, board, label, unicode)
+  end
+
+  def make_move(target_r, target_c)
+    dup_board = board.clone
+    dup_board.board = deep_dup(dup_board.board)
+
+    if @board[target_r, target_c].nil? || @board[target_r, target_c].color != color
+      board[target_r, target_c] = self
+      board[row, col] = nil
+      self.row = target_r
+      self.col = target_c
+    else
+      raise ArgumentError.new("Invalid Move")
+    end
+  end
+
+  def valid_move?(target_r, target_c)
+    two_and_one = (row - target_r).abs == 1 && (col - target_c).abs == 2
+    one_and_two = (row - target_r).abs == 2 && (col - target_c).abs == 1
+    two_and_one || one_and_two
+  end
+end
