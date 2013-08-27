78ac52fca0a1ef671f837a54f4938678baf65a36
App Academy Student <student@appacademy.io>
2013-08-27 10:29:21 -0400

    Adds display feature, and begins move for pieces.

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require './piece.rb'
+
 class Board
   #COLUMNS = [:a..:h]
 
@@ -5,6 +7,7 @@ class Board
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
+    self[0, 0] = Rook.new(:white, 0, 0, self)
   end
 
   def [](row, col)
@@ -15,6 +18,24 @@ class Board
   def []=(row, col, value)
     #self.board[(num - 8).abs][COLUMNS.index(let)] = value
     self.board[row][col] = value
+  end
+
+  def display
+    header = 8
+    board.each do |row|
+      print "#{header} "
+      row.each do |piece|
+        if piece
+          print "#{piece.label} "
+        else
+          print "  "
+        end
+      end
+      print "\n"
+      header -= 1
+    end
+
+    print "  a b c d e f g h"
   end
 
 end
--- piece.rb
+++ piece.rb
@@ -1,21 +1,25 @@
 class Piece
-  attr_accessor :color, :row, :col, :board
+  attr_accessor :color, :row, :col, :board, :label
 
-  def initialize(color, row, col, board)
+  def initialize(color, row, col, board, label)
     @color = color
     @row = row
     @col = col
     @board = board
+    @label = label
   end
 
   def move(new_row, new_col)
-    if actual_move?
-    #check if valid
-    #move it
+    if actual_move?(new_row, new_col) && move_valid?(new_row, new_col)
+      @board[new_row, new_col] = self
+      @board[row, col] = nil
+      self.row = new_row
+      self.col = new_col
+    end
   end
 
   def actual_move?(new_row, new_col)
-     new_row != row && new_col != col
+     new_row != row || new_col != col
   end
 
 
@@ -23,11 +27,19 @@ end
 
 
 class Rook < Piece
+
+  def initialize(color, row, col, board)
+    color == :black ? label = :R : label = :r
+    super(color, row, col, board, label)
+  end
+
   def move_valid?(new_row, new_col)
     if new_row == @row
-      [new_row...row].
-
+      return true
     elsif new_col == @col
+      return true
+    end
+    false
   end
 
 end
